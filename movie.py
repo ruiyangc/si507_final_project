a27bc0c2181f3d6bd305fdbde4469f8f8a366fd4
Ray Chang <raychang@253-3.synergydsl.com>
2020-04-27 12:36:01 -0400

    Add .gitignore

---
+++ movie.py
@@ -0,0 +1,9 @@
+#########################################
+##### Name: Ruiyang Chang           #####
+##### Uniqname: ruiyangc            #####
+##### SI507 WN20 Final Project      #####
+#########################################
+
+import json
+import requests
+import secrets # file that contains your OAuth credentials
